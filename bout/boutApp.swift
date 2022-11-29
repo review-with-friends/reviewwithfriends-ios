bcf095150767db54d1a9acf2479af7d1c6070411
colathro <colton@spacedoglabs.com>
2022-11-28 16:06:21 -0800

    Initial Commit

---
+++ bout/boutApp.swift
@@ -0,0 +1,17 @@
+//
+//  boutApp.swift
+//  bout
+//
+//  Created by Colton Lathrop on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct boutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView().preferredColorScheme(.dark)
+        }
+    }
+}
