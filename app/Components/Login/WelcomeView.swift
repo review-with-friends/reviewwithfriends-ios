7978f72dfb751a6523251f7bf1d84b19c5d88a19
colton@spacedoglabs.com <colton@spacedoglabs.com>
2023-05-03 12:15:27 -0700

    add links to eula

--- app/Components/Login/WelcomeView.swift
+++ app/Components/Login/WelcomeView.swift
@@ -31,13 +31,30 @@ struct WelcomeView: View {
                 VStack {
                     Spacer()
                     VStack {
-                        Text("Review With Friends").font(.system(size: 78).bold()).padding()
+                        Text("Review with friends").font(.system(size: 78).bold()).padding()
                     }.background(.black).cornerRadius(16).shadow(radius: 8)
                     Spacer()
                     Spacer()
                 }
             }
-            Text("By pressing Get Started, you are agreeing to our Terms of Service and Privacy policy.").font(.caption).foregroundColor(.secondary).padding(.horizontal).multilineTextAlignment(.center)
+            VStack {
+                Text("By pressing Get Started, you agree to our policies:").font(.caption).foregroundColor(.secondary).padding(.horizontal).multilineTextAlignment(.center)
+                HStack {
+                    Link(destination: URL(string: "https://reviewwithfriends.com/TERMSOFUSE.pdf")!){
+                        Text("Terms of Use").font(.caption).foregroundColor(.secondary).underline()
+                    }
+                    Link(destination: URL(string: "https://reviewwithfriends.com/PRIVACYPOLICY.pdf")!){
+                        Text("Privacy Policy").font(.caption).foregroundColor(.secondary).underline()
+                    }
+                    Link(destination: URL(string: "https://reviewwithfriends.com/EULA.pdf")!){
+                        Text("EULA").font(.caption).foregroundColor(.secondary).underline()
+                    }
+                    Link(destination: URL(string: "https://reviewwithfriends.com/COMMUNITYGUIDELINES.pdf")!){
+                        Text("Community Guidelines").font(.caption).foregroundColor(.secondary).underline()
+                    }
+                }.padding(.top, 8)
+            }
+
             PrimaryButton(title: "Get Started", action: {
                 self.path.append(GetCode())
             }).padding(.vertical)
